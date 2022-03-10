80775bf1482da9ce8cd1f7a628df20d14ece1523
AmberRoseCodes <amber.hilton@google.com>
2022-03-10 21:30:34 +0000

    api added

--- cgi-biocomp2/bl/blapi_dummy.py
+++ cgi-biocomp2/bl/blapi_dummy.py
@@ -23,3 +23,61 @@ def getAllEntries():
     """
     return(dbapi.getAllEntries())
 
+
+#---------------------------------
+# FRONT PAGE VARIABLES
+
+# Identifier  (eg. BAA22866)
+
+def id():
+    id='BAA22866'
+    return(id)
+
+# Protein Product Name  (eg. glycosylphosphatidylinositol)
+
+def ppn():
+    ppn='glycosylphosphatidylinositol'
+    return(ppn)
+
+# Genbank Accession  (eg. ABB08360)
+
+def genbank_acc():
+    genbank_acc='ABB08360'
+    return(genbank_acc)
+
+# Chromosomal Accession  (eg. 8q24.3)
+
+def chrom_acc():
+    chrom_acc='8q24.3'
+    return(chrom_acc)
+
+#---------------------------------------
+# DETAIL PAGE VARIABLES
+
+# CDS Coding Region / Sticky end restriction enzymes / full origin sequence
+
+def CDS_DNA_string():
+    CDS_DNA_string='gatcctccat atacaacggt atctccacct caggtttaga tctcaacaac ggaaccattg'
+    return(CDS_DNA_string)
+
+def sticky_ends():
+    sticky_end_EcoRI=[10, 20]
+    sticky_end_BamHI=[5, 18]
+    sticky_end_BsuMI=[8, 22]
+    sticky_end_optional=[15, 25]
+    return(sticky_EndEcoRI, sticky_end_BamHI, sticky_end_BsuMI, sticky_end_optional)
+
+# CDS Amino Acid Seq
+
+def CDS_aa_string():
+    CDS_aa_string='MNRWVEKWLRVYLKCYINLILFYRNVYPPQSFDYTTYQSFNLPQ'
+    return(CDS_aa_string)
+
+# Entry Codon Useage Frequence / Whole chormosome Codon Useage Frequency
+
+def codon_useage():
+    A = [['UUU', 'UUG', 'UGA', 'TAG'], 
+    [1.18, 0.8, 1.9, 0.5],
+    [2, 0.8, 1.1, 1.9]]
+
+
