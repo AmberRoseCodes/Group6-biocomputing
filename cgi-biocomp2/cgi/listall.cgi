9ac1fe902ec332c85315579761d50c908ca4dd06
AmberRoseCodes <amber.hilton@google.com>
2022-04-07 23:03:11 +0100

    Thursdays session

--- cgi-biocomp2/cgi/listall.cgi
+++ cgi-biocomp2/cgi/listall.cgi
@@ -19,15 +19,18 @@ import config     # Import configuration information (e.g. URLs)
 entries = blapi_dummy.getAllEntries()
 html    = htmlutils.header()
 
-
-
 html += "  <table>\n"
+html += "<tr><th style='background-color: yellow'>Accession Number</th>"
+html += "<th>Gene ID</th>"
+html += "<th>Protien Product Name</th>"
+html += "<th>Chromosomal Location</th></tr>\n"
 
-for entry in entries:
-    html += "    <tr><td>"
-    html += "<a href='" + config.searchurl + "?ac=" + entry + "'>"
-    html += entry + "</a>"
-    html += "</td></tr>\n"
+for d in entries:
+        html += "<tr><td>" + "<a href='" + config.searchurl + "?ac=" + d.get('acc') + "'>"+ d.get('acc') + "</a>""</td>"
+        html += "<td>" + d.get('gene_id')+ "</td>"
+        html += "<td>" + d.get('ppn')+ "</td>"
+        html += "<td>" + d.get('chrom_loc')+ "</td></tr>"
+
 
     
 html += "  </table>\n"
@@ -36,4 +39,11 @@ html += htmlutils.footer()
 print(html)
 
 
+#<td>Hi, I'm your first cell.</td>
+#Copy to Clipboard
+#If we want a row of four cells, we need to copy these tags three times. Update the contents of your table to look like so:
+#<td>Hi, I'm your first cell.</td>
+#<td>I'm your second cell.</td>
+#<td>I'm your third cell.</td>
+#<td>I'm your fourth cell.</td>
 
--- cgi-biocomp2/db/dbapi_dummy.py
+++ cgi-biocomp2/db/dbapi_dummy.py
@@ -9,7 +9,7 @@ This is the database API - it needs to access the MySQL database
 import sys
 sys.path.insert(0, "../")
 
-import config  # Import configuration information (e.g. database connection)
+#import config  # Import configuration information (e.g. database connection)
 
 def getAllEntries():
     """
@@ -19,4 +19,30 @@ def getAllEntries():
     return a list of dictionaries and would access the MySQL database
     """
 
-    return(['AB000123', 'AB000321', 'AC001564'])
+    return([{'acc':'AB000321', 'gene_id':'gh56', 'ppn':'glucoselipid', 'chrom_loc':'103..600'},{'acc':'AB000322', 'gene_id':'gkk56', 'ppn':'glucoselipid-2', 'chrom_loc':'203..650'}])
+
+
+
+# whole genome DNA String  (eg. gatcctccat)
+
+
+def whole_chromosome():
+    whole_chromosome = 'gatcctccat atacaacggt atctccacct caggtttaga tctcaacaac ggaaccattg gatcctccat atacaacggt atctccacct caggtttaga tctcaacaac ggaaccattg gatcctccat atacaacggt atctccacct caggtttaga tctcaacaac ggaaccattg'
+    return(whole_chromosome)
+
+# list of codons (eg. [UUU,UUG,UUA])
+
+def codons():
+    codons=['UUU','UUG','UUA']
+    return(codons)
+
+
+# Restriction enzyme sequence dictionary (eg. [UUU,UUG,UUA])
+
+#def restriction_enz():
+#    restriction_enz=['EcoRI':'gattc', 'BamHI':'ggcct','BsuMI':'ccctta']
+#    return(restriction_enz)
+
+
+
+
